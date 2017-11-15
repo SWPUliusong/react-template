428390a0efb63b0bc873f191d1cb87ec366d1709
liusong <swpuliusong@foxmail.com>
2017-11-15 15:03:15 +0800

    update

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import "./assets/index.css"
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './components/App'
+
+const render = Component => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        document.getElementById('app'),
+    )
+}
+
+render(App)
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+    module.hot.accept('./components/App', () => { render(App) })
+}
